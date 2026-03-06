46b3b11f32f144a2bb1f58bcd4b897ce87b53087
psgus <seunghyeon8721@gmail.com>
2026-03-06 21:37:18 +0900

    feat(week2-string): IPv6 문제 풀이

--- week2/problem-solving/난이도중_문자열_IPv6_실버1.py
+++ week2/problem-solving/난이도중_문자열_IPv6_실버1.py
@@ -1,2 +1,13 @@
-# 문자열 - IPv6 (백준 실버1)
-# 문제 링크: https://www.acmicpc.net/problem/3107
+# String - IPv6 (BOJ 3107)
+
+address = input().strip()
+
+if "::" in address:
+    left, right = address.split("::")
+    left_groups = left.split(":") if left else []
+    right_groups = right.split(":") if right else []
+    groups = left_groups + ["0"] * (8 - len(left_groups) - len(right_groups)) + right_groups
+else:
+    groups = address.split(":")
+
+print(":".join(group.zfill(4) for group in groups))
