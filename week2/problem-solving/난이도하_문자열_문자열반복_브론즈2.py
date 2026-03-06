015c3eb31d1aa89b19d67c81ea109e95192cfc52
psgus <seunghyeon8721@gmail.com>
2026-03-06 20:14:33 +0900

    feat(week2-string): 문자열 반복 문제 풀이

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,7 @@
-# 문자열 - 문자열 반복 (백준 브론즈2)
-# 문제 링크: https://www.acmicpc.net/problem/2675
+# String - Repeating Characters (BOJ 2675)
+
+t = int(input())
+
+for _ in range(t):
+    repeat, text = input().split()
+    print("".join(char * int(repeat) for char in text))
