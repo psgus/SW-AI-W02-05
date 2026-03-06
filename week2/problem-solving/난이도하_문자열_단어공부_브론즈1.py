e6292d4a5161b4bb20186e07b7c591333d09492a
psgus <seunghyeon8721@gmail.com>
2026-03-06 19:04:51 +0900

    feat(week2-string): 단어공부 문제 풀이

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,14 @@
-# 문자열 - 단어 공부 (백준 브론즈1)
-# 문제 링크: https://www.acmicpc.net/problem/1157
+# String - Word Study (BOJ 1157)
+
+word = input().strip().upper()
+counts = [0] * 26
+
+for char in word:
+    counts[ord(char) - ord("A")] += 1
+
+max_count = max(counts)
+
+if counts.count(max_count) > 1:
+    print("?")
+else:
+    print(chr(counts.index(max_count) + ord("A")))
