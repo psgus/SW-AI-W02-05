238e74d5b29bb9fee1993d1c240b5a5ed07daac1
psgus <seunghyeon8721@gmail.com>
2026-03-07 19:38:12 +0900

    feat(week2-array): 평균은 넘겠지 문제 풀이

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,11 @@
-# 배열 - 평균은 넘겠지 (백준 브론즈1)
-# 문제 링크: https://www.acmicpc.net/problem/4344
+# Array - Above Average (BOJ 4344)
+
+c = int(input())
+
+for _ in range(c):
+    data = list(map(int, input().split()))
+    n = data[0]
+    scores = data[1:]
+    average = sum(scores) / n
+    above = sum(score > average for score in scores)
+    print(f"{above / n * 100:.3f}%")
