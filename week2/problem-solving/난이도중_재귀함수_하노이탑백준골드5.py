0acfa9960e72cd95e6b3a9301fd9ae9aec893516
psgus <seunghyeon8721@gmail.com>
2026-03-08 21:06:45 +0900

    feat(week2-recursion): 하노이 탑 문제 풀이

--- week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
+++ week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
@@ -1,2 +1,17 @@
-# 재귀함수 - 하노이 탑 (백준 골드5)
-# 문제 링크: https://www.acmicpc.net/problem/1914
+# Recursion - Tower of Hanoi (BOJ 1914)
+
+n = int(input())
+print(2 ** n - 1)
+
+
+def move(count, start, end, via):
+    if count == 0:
+        return
+
+    move(count - 1, start, via, end)
+    print(start, end)
+    move(count - 1, via, end, start)
+
+
+if n <= 20:
+    move(n, 1, 3, 2)
