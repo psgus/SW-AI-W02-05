ff73a6a4ee1b33e37b7dbb7e682fb4e0ef378ed0
psgus <seunghyeon8721@gmail.com>
2026-03-10 21:41:16 +0900

    feat(week2-backtracking): 외판원 순회 2 문제 풀이

--- week2/problem-solving/난이도중_백트래킹_외판원순회2_실버2.py
+++ week2/problem-solving/난이도중_백트래킹_외판원순회2_실버2.py
@@ -1,2 +1,29 @@
-# 백트래킹 - 외판원 순회 2 (백준 실버2)
-# 문제 링크: https://www.acmicpc.net/problem/10971
+# Backtracking - Traveling Salesman 2 (BOJ 10971)
+
+n = int(input())
+cost = [list(map(int, input().split())) for _ in range(n)]
+visited = [False] * n
+answer = 10 ** 9
+
+
+def dfs(start, current, depth, total):
+    global answer
+
+    if total >= answer:
+        return
+
+    if depth == n:
+        if cost[current][start] != 0:
+            answer = min(answer, total + cost[current][start])
+        return
+
+    for next_city in range(n):
+        if not visited[next_city] and cost[current][next_city] != 0:
+            visited[next_city] = True
+            dfs(start, next_city, depth + 1, total + cost[current][next_city])
+            visited[next_city] = False
+
+
+visited[0] = True
+dfs(0, 0, 1, 0)
+print(answer)
