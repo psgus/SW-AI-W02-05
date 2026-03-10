f686136640707bbb08f1568fb6216eb5212d5193
psgus <seunghyeon8721@gmail.com>
2026-03-10 20:22:54 +0900

    feat(week2-backtracking): N-Queen 문제 풀이

--- week2/problem-solving/난이도중_백트래킹_NQueen_골드4.py
+++ week2/problem-solving/난이도중_백트래킹_NQueen_골드4.py
@@ -1,2 +1,23 @@
-# 백트래킹 - N-Queen (백준 골드4)
-# 문제 링크: https://www.acmicpc.net/problem/9663
+# Backtracking - N-Queen (BOJ 9663)
+
+n = int(input())
+answer = 0
+
+
+def dfs(row, columns, diag_left, diag_right):
+    global answer
+
+    if row == n:
+        answer += 1
+        return
+
+    available = ((1 << n) - 1) & ~(columns | diag_left | diag_right)
+
+    while available:
+        bit = available & -available
+        available -= bit
+        dfs(row + 1, columns | bit, (diag_left | bit) << 1, (diag_right | bit) >> 1)
+
+
+dfs(0, 0, 0, 0)
+print(answer)
