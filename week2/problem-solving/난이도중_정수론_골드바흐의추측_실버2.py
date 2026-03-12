63f96a35821d70610488371c644639dc9b47c1d7
psgus <seunghyeon8721@gmail.com>
2026-03-12 22:15:07 +0900

    feat(week2-number-theory): 골드바흐의 추측 문제 풀이

--- week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
+++ week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
@@ -1,2 +1,25 @@
-# 정수론 - 골드바흐의 추측 (백준 실버2)
-# 문제 링크: https://www.acmicpc.net/problem/9020
+# Number Theory - Goldbach's Conjecture (BOJ 9020)
+
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+numbers = [int(input()) for _ in range(t)]
+limit = max(numbers)
+is_prime = [True] * (limit + 1)
+is_prime[0] = False
+is_prime[1] = False
+
+for number in range(2, int(limit ** 0.5) + 1):
+    if is_prime[number]:
+        for multiple in range(number * number, limit + 1, number):
+            is_prime[multiple] = False
+
+for number in numbers:
+    for left in range(number // 2, 1, -1):
+        right = number - left
+
+        if is_prime[left] and is_prime[right]:
+            print(left, right)
+            break
