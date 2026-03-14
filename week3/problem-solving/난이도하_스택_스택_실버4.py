b9d55adba163b5ed6641e7c5719f0d4b07423522
psgus <seunghyeon8721@gmail.com>
2026-03-14 19:44:36 +0900

    feat(week3-stack): 스택 문제 풀이

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,25 @@
-# 스택 - 스택 (백준 실버 4)
-# 문제 링크: https://www.acmicpc.net/problem/10828
+# Stack - Stack (BOJ 10828)
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+answer = []
+
+for _ in range(n):
+    command = input().split()
+
+    if command[0] == "push":
+        stack.append(command[1])
+    elif command[0] == "pop":
+        answer.append(stack.pop() if stack else "-1")
+    elif command[0] == "size":
+        answer.append(str(len(stack)))
+    elif command[0] == "empty":
+        answer.append("0" if stack else "1")
+    else:
+        answer.append(stack[-1] if stack else "-1")
+
+print("\n".join(answer))
