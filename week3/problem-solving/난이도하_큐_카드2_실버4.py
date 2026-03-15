3a7a311b6cc06f1b2c053f04b6fd44105510b73b
psgus <seunghyeon8721@gmail.com>
2026-03-15 20:16:49 +0900

    feat(week3-queue): 카드2 문제 풀이

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,12 @@
-# 큐 - 카드2 (백준 실버4)
-# 문제 링크: https://www.acmicpc.net/problem/2164
+# Queue - Card 2 (BOJ 2164)
+
+from collections import deque
+
+n = int(input())
+cards = deque(range(1, n + 1))
+
+while len(cards) > 1:
+    cards.popleft()
+    cards.append(cards.popleft())
+
+print(cards[0])
