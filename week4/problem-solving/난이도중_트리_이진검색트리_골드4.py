9db6543a9804bc81170bd65215952b1dfe787115
psgus <seunghyeon8721@gmail.com>
2026-03-20 22:05:13 +0900

    feat(week4-tree): 이진 검색 트리 문제 풀이

--- week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
+++ week4/problem-solving/난이도중_트리_이진검색트리_골드4.py
@@ -1,2 +1,30 @@
-# 트리 - 이진 검색 트리 (백준 골드4)
-# 문제 링크: https://www.acmicpc.net/problem/5639
+# Tree - Binary Search Tree (BOJ 5639)
+
+import sys
+
+sys.setrecursionlimit(100000)
+
+preorder = list(map(int, sys.stdin.read().split()))
+index = 0
+postorder = []
+
+
+def build_postorder(lower, upper):
+    global index
+
+    if index >= len(preorder):
+        return
+
+    value = preorder[index]
+
+    if not (lower < value < upper):
+        return
+
+    index += 1
+    build_postorder(lower, value)
+    build_postorder(value, upper)
+    postorder.append(value)
+
+
+build_postorder(float("-inf"), float("inf"))
+print("\n".join(map(str, postorder)))
