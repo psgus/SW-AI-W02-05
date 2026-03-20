e1229c8393552eb8213b4a10414932a4d98e2572
psgus <seunghyeon8721@gmail.com>
2026-03-20 20:37:41 +0900

    feat(week4-tree): 상근이의 여행 문제 풀이

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,18 @@
-# 트리 - 상근이의 여행 (백준 실버4)
-# 문제 링크: https://www.acmicpc.net/problem/9372
+# Tree - Sanggeun's Travel (BOJ 9372)
+
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+answer = []
+
+for _ in range(t):
+    n, m = map(int, input().split())
+
+    for _ in range(m):
+        input()
+
+    answer.append(str(n - 1))
+
+print("\n".join(answer))
