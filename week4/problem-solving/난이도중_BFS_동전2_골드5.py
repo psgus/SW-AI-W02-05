af4d4fe4b961f91e2c8fed4322aefb2365fd05e4
psgus <seunghyeon8721@gmail.com>
2026-03-21 21:46:08 +0900

    feat(week4-dp): 동전 2 문제 풀이

--- week4/problem-solving/난이도중_BFS_동전2_골드5.py
+++ week4/problem-solving/난이도중_BFS_동전2_골드5.py
@@ -1,2 +1,17 @@
-# BFS - 동전 2 (백준 골드5)
-# 문제 링크: https://www.acmicpc.net/problem/2294
+# DP - Coin 2 (BOJ 2294)
+
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = sorted({int(input()) for _ in range(n)})
+inf = 10 ** 9
+dp = [inf] * (k + 1)
+dp[0] = 0
+
+for coin in coins:
+    for amount in range(coin, k + 1):
+        dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+print(dp[k] if dp[k] != inf else -1)
