bf8d7caa71251fee57a6379bf875125538c89a7a
psgus <seunghyeon8721@gmail.com>
2026-03-23 20:28:44 +0900

    feat(week4-graph): 연결 요소의 개수 문제 풀이

--- week4/problem-solving/난이도중_그래프DFSBFS_연결요소의개수_실버2.py
+++ week4/problem-solving/난이도중_그래프DFSBFS_연결요소의개수_실버2.py
@@ -1,2 +1,34 @@
-# 그래프, DFS, BFS - 연결 요소의 개수 (백준 실버2)
-# 문제 링크: https://www.acmicpc.net/problem/11724
+# Graph - Number of Connected Components (BOJ 11724)
+
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (n + 1)
+count = 0
+
+for start in range(1, n + 1):
+    if visited[start]:
+        continue
+
+    count += 1
+    stack = [start]
+    visited[start] = True
+
+    while stack:
+        node = stack.pop()
+
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                stack.append(next_node)
+
+print(count)
