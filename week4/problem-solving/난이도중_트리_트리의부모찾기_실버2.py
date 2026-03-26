0aff9d0902e1e2c8fd9dea54b1c64688f081b214
Hyeon <seunghyeon8721@gmail.com>
2026-03-26 21:46:18 +0900

    3/26 update

--- week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
+++ week4/problem-solving/난이도중_트리_트리의부모찾기_실버2.py
@@ -1,2 +1,34 @@
 # 트리 - 트리의 부모 찾기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/11725
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+def bfs(graph, start=1):
+    queue = deque([start])
+    visited = set([start])
+    parent = [0]*(n+1)
+
+    while queue:
+        a = queue.popleft()
+
+        for node in graph[a]:
+            if node not in visited:
+                visited.add(node)
+                parent[node] = a
+                queue.append(node)
+
+    return parent
+
+p = bfs(graph, start=1)
+
+for i in range(2, n+1):
+    print(p[i])
--- week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,31 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False] * (n + 1)
+
+
+def dfs(v):
+    visited[v] = True
+    count = 1
+
+    for next in graph[v]:
+        if not visited[next]:
+            count += dfs(next)
+
+    return count
+
+
+print(dfs(1) - 1)
