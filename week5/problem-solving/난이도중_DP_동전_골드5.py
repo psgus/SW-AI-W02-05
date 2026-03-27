8e7e89b4395b386361b75abde27565642b4ac7d5
psgus <seunghyeon8721@gmail.com>
2026-03-27 21:02:46 +0900

    feat(week5-dp): 동전 문제 풀이

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,23 @@
-# DP - 동전 (백준 골드5)
-# 문제 링크: https://www.acmicpc.net/problem/9084
+# DP - Coin (BOJ 9084)
+
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+answer = []
+
+for _ in range(t):
+    n = int(input())
+    coins = list(map(int, input().split()))
+    target = int(input())
+    dp = [0] * (target + 1)
+    dp[0] = 1
+
+    for coin in coins:
+        for amount in range(coin, target + 1):
+            dp[amount] += dp[amount - coin]
+
+    answer.append(str(dp[target]))
+
+print("\n".join(answer))
