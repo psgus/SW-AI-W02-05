9764ba8bd5c60c1c1b1f9179c14b0d1fe6592be5
psgus <seunghyeon8721@gmail.com>
2026-03-27 19:35:28 +0900

    feat(week5-dp): 01타일 문제 풀이

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,18 +1,14 @@
-# DP - 01타일 (백준 실버3)
-# 문제 링크: https://www.acmicpc.net/problem/1904
-import sys
-input = sys.stdin.readline
+# DP - 01 Tile (BOJ 1904)
 
 n = int(input())
 
-def tile(n):
-    dp=[0]*(n+1)
-    dp[1] = 1
-    dp[2] = 2
-    if n == 1:
-        return 1
-    for i in range(3,n+1):
-        dp[i]=(dp[i-1]+dp[i-2]) % 15746
-    return dp[n]
-result = tile(n)
-print(result)
+if n == 1:
+    print(1)
+else:
+    prev2 = 1
+    prev1 = 2
+
+    for _ in range(3, n + 1):
+        prev2, prev1 = prev1, (prev1 + prev2) % 15746
+
+    print(prev1)
