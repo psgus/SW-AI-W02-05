be37daecb2ee93a5068b89729af07d7bfa841589
psgus <seunghyeon8721@gmail.com>
2026-03-28 19:48:31 +0900

    feat(week5-dp): LCS 문제 풀이

--- week5/problem-solving/난이도중_DP_LCS_골드5.py
+++ week5/problem-solving/난이도중_DP_LCS_골드5.py
@@ -1,2 +1,20 @@
-# DP - LCS (백준 골드5)
-# 문제 링크: https://www.acmicpc.net/problem/9251
+# DP - LCS (BOJ 9251)
+
+a = input().strip()
+b = input().strip()
+dp = [0] * (len(b) + 1)
+
+for char_a in a:
+    previous = 0
+
+    for i, char_b in enumerate(b, start=1):
+        temp = dp[i]
+
+        if char_a == char_b:
+            dp[i] = previous + 1
+        else:
+            dp[i] = max(dp[i], dp[i - 1])
+
+        previous = temp
+
+print(dp[-1])
