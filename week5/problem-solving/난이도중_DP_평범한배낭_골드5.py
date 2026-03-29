7cc9bc3478b9583f4291e5e814c1c13554a71330
psgus <seunghyeon8721@gmail.com>
2026-03-29 20:17:52 +0900

    feat(week5-knapsack): 평범한 배낭 문제 풀이

--- week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
+++ week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
@@ -1,2 +1,16 @@
-# DP - 평범한 배낭 (백준 골드5)
-# 문제 링크: https://www.acmicpc.net/problem/12865
+# DP - Ordinary Knapsack (BOJ 12865)
+
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+dp = [0] * (k + 1)
+
+for _ in range(n):
+    weight, value = map(int, input().split())
+
+    for capacity in range(k, weight - 1, -1):
+        dp[capacity] = max(dp[capacity], dp[capacity - weight] + value)
+
+print(dp[k])
