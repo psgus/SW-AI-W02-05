948f800569bca518962710b5881f38b103b423f6
psgus <seunghyeon8721@gmail.com>
2026-04-01 19:54:17 +0900

    feat(week5-greedy): 신입사원 문제 풀이

--- week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -1,2 +1,25 @@
-# 그리디 - 신입 사원 (백준 실버1)
-# 문제 링크: https://www.acmicpc.net/problem/1946
+# Greedy - New Recruits (BOJ 1946)
+
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+answer = []
+
+for _ in range(t):
+    n = int(input())
+    applicants = [tuple(map(int, input().split())) for _ in range(n)]
+    applicants.sort()
+
+    hired = 0
+    best_interview = n + 1
+
+    for _, interview_rank in applicants:
+        if interview_rank < best_interview:
+            hired += 1
+            best_interview = interview_rank
+
+    answer.append(str(hired))
+
+print("\n".join(answer))
