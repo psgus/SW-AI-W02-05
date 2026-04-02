485c781c3b8853fff309df8d57881e14b5177ab8
psgus <seunghyeon8721@gmail.com>
2026-04-02 20:28:52 +0900

    feat(week5-greedy): 회의실 배정 문제 풀이

--- week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
+++ week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
@@ -1,2 +1,19 @@
-# 그리디 - 회의실 배정 (백준 골드5)
-# 문제 링크: https://www.acmicpc.net/problem/1931
+# Greedy - Meeting Room Assignment (BOJ 1931)
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+meetings = [tuple(map(int, input().split())) for _ in range(n)]
+meetings.sort(key=lambda meeting: (meeting[1], meeting[0]))
+
+count = 0
+end_time = 0
+
+for start, end in meetings:
+    if start >= end_time:
+        count += 1
+        end_time = end
+
+print(count)
